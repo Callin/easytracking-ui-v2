8f8e644abcb30ea451ff4e131e8c1e6f4fef2b97
Dragos Sabau <sabau.dragos@gmail.com>
2018-02-26 22:37:10 +0200

    Added signup and signin component. Upgraded the dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {SiginComponent} from './auth/sigin/sigin.component';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SiginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
